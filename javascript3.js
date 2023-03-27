a1331bbb4f9b4722d2d736288f25b53c3152602c
louvalman <louis.dyrhauge@gmail.com>
2023-03-27 10:02:04 +0200

    create blank placeholder text ,'' for doodle4 prompts

--- javascript3.js
+++ javascript3.js
@@ -1,7 +1,7 @@
-let userName = prompt("Who's there"); // Create prompt that asks user to enter their name, sets their input in a variable called userName
+let userName = prompt("Who's there", ''); // Create prompt that asks user to enter their name, sets their input in a variable called userName
 
 if (userName === 'Admin') {
-    let pass = prompt('Password?'); // If the user replies the prompt with Admin, ask in a new prompt for a password
+    let pass = prompt('Password?', ''); // If the user replies the prompt with Admin, ask in a new prompt for a password
     if (pass === 'TheMaster') { // If password prompt is answered with TheMaster,
         alert('Welcome!') // give user an alert that says Welcome!
     } else if (pass === '' || pass === null) { // If the user reply with an empty password, or cancels the prompt,
