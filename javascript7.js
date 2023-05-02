42bed11997a95b913982091dbf4e2736c909e66f
louvalman <94385943+louvalman@users.noreply.github.com>
2023-05-02 22:45:10 +0200

    Create playRound function that evaluates user and computer input and finds winner!

--- javascript7.js
+++ javascript7.js
@@ -1,14 +1,42 @@
+let Player;
+let Computer;
+
 let getComputerChoice = () => {
-    let choiceGenerator = Math.floor(Math.random() * 100) + 1;
-    if (choiceGenerator <= 33) {
+    let randomNumber = Math.floor(Math.random() * 100) + 1;
+    if (randomNumber <= 33) {
         getComputerChoice = "Rock"; 
-    } else if (choiceGenerator <= 66) {
+    } else if (randomNumber <= 66) {
         getComputerChoice = "Paper";
     } else {
         getComputerChoice = "Scissor";
     }
-    console.log(choiceGenerator);
     return getComputerChoice;
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    computerSelection = getComputerChoice;
+    let winner;
+    if (playerSelection = "Rock" && getComputerChoice === "Paper") {
+        return 'You lose! Paper beats Rock.'
+    } else if (playerSelection = "Rock" && getComputerChoice === "Scissor") {
+        return 'You win! Rock beats Scissor.'
+    } else if (playerSelection = "Rock" && getComputerChoice === "Rock") {
+        return 'It is a tie! Play again.'
+    } else if (playerSelection = "Paper" && getComputerChoice === "Rock") {
+        return 'You win! Paper beats rock.'
+    } else if (playerSelection = "Paper" && getComputerChoice === "Paper") {
+        return 'It is a tie! Play again!'
+    } else if (playerSelection = "Paper" && getComputerChoice === "Scissor") {
+        return 'You lose! Scissor beats paper.'
+    } else if (playerSelection = "Scissor" && getComputerChoice === "Rock") {
+        return 'You lose! Rock beats Scissor.'
+    } else if (playerSelection = "Scissor" && getComputerChoice === "Paper") {
+        return 'You win! Scissor beats paper'
+    } else if (playerSelection = "Scissor" && getComputerChoice === "Scissor") {
+        return 'It is a tie! Play again!'
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
