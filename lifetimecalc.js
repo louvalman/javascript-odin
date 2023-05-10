2b34fa3ec42b6fda635b478d10bb44c4946cb820
louvalman <louis.dyrhauge@gmail.com>
2023-05-10 15:58:51 +0200

    single quotes prettier config change

--- lifetimecalc.js
+++ lifetimecalc.js
@@ -1,16 +1,16 @@
-const postSupply = document.querySelector(".para");
+const postSupply = document.querySelector('.para');
 let age;
 let amount;
 
 function calculateSupply(age, amount) {
   age = parseFloat(
-    prompt("How old are you?", "Type your age").replace(",", ".")
+    prompt('How old are you?', 'Type your age').replace(',', '.')
   );
   amount = parseFloat(
     prompt(
-      "How many do you need pr. day of the product?",
-      "Type your average pr. day"
-    ).replace(",", ".")
+      'How many do you need pr. day of the product?',
+      'Type your average pr. day'
+    ).replace(',', '.')
   );
   const maxAge = 80;
   let finalDays = (maxAge - age) * 365; // finds your remaining years, and multiplies with 365 to find days left to live until eighty
--- puppyagecalc.js
+++ puppyagecalc.js
@@ -1,5 +1,5 @@
-const postAge = document.querySelector(".para");
-const age = prompt("What's the age of your dog in human years?", "");
+const postAge = document.querySelector('.para');
+const age = prompt("What's the age of your dog in human years?", '');
 
 function calculateDogAge(age) {
   dogYears = age * 7;
