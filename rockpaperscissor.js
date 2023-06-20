32d05595f1164a33b6abf449e1e6db2c09c39d56
louvalman <louisdyrhauge@hotmail.com>
2023-06-20 20:03:58 +0200

    styles and aos

--- rockpaperscissor.js
+++ rockpaperscissor.js
@@ -34,6 +34,12 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
+const removeRoundCountPlaceholder = () => {
+  roundContainer.removeChild(roundCountPlaceholder);
+};
+
+let roundContainer = document.querySelector('.roundContainer');
+let roundCountPlaceholder = document.querySelector('.roundCountPlaceholder');
 let score = document.querySelector('.score');
 let playerCompScore = document.querySelector('.playerCompScore');
 let playerScore = 0;
@@ -51,6 +57,9 @@ const buttons = document.querySelectorAll('button');
 
 buttons.forEach((button) => {
   button.addEventListener('click', (e) => {
+    if (roundCountPlaceholder.parentNode === roundContainer) {
+      removeRoundCountPlaceholder();
+    }
     const playerSelection = e.target.id;
     const computerSelection = getComputerChoice();
 
@@ -60,37 +69,41 @@ buttons.forEach((button) => {
       // calculate the winner by incrementing the player's or computer's score by looking at the winner variable defined in the playRound function
       playerScore++;
       roundStatus.textContent = `For humanity! You win the round with ${playerSelection} against ${computerSelection}.`;
+      roundStatus.style.color = 'lightgreen';
     } else if (roundResult.winner === 'computer') {
       computerScore++;
       roundStatus.textContent = `Damn machines! You lost the round with ${playerSelection} against ${computerSelection}.`;
+      roundStatus.style.color = 'lightcoral';
     } else if (roundResult.winner === null) {
       tieCounter++;
       roundStatus.textContent = `It's a tie. Both you and the machines picked ${playerSelection}.`;
+      roundStatus.style.color = 'lightblue';
     }
 
     let roundScore = playerScore + computerScore + tieCounter;
 
-    roundCount.textContent = `Round number: ${roundScore}`;
+    roundCount.textContent = `Round ${roundScore}`;
     roundCount.classList.add('roundNumber');
-    playerCompScore.appendChild(roundCount);
+    roundContainer.append(roundCount);
 
-    playerResult.textContent = 'Your score: ' + playerScore;
+    playerCompScore.appendChild(roundStatus);
+    roundStatus.classList.add('fade-in-animation');
+
+    playerResult.textContent = `Your score: ${playerScore} - Computer score: ${computerScore}`;
+
     playerCompScore.appendChild(playerResult);
 
-    computerResult.textContent = 'Computer score: ' + computerScore;
-    playerCompScore.appendChild(computerResult);
-
-    score.appendChild(roundStatus);
-    roundStatus.classList.add('fade-in-animation');
+    // computerResult.textContent = 'Computer score: ' + computerScore;
+    // playerCompScore.appendChild(computerResult);
 
     const endGame = () => {
       if (computerScore > playerScore) {
         gameResult.textContent =
           'Oh no, the invasion has begun. Humanity is lost.';
-        gameResult.style.color = 'red';
+        gameResult.style.color = 'lightcoral';
       } else if (playerScore > computerScore) {
         gameResult.textContent = 'You did it! Humanity is saved!';
-        gameResult.style.color = 'green';
+        gameResult.style.color = 'lightgreen';
       } else if (playerScore === computerScore) {
         gameResult.textContent =
           "It's a tie! Not the kind you tie around your neck.";
@@ -130,11 +143,11 @@ const newGame = () => {
   buttons.forEach((button) => {
     button.removeAttribute('disabled', '');
   });
-  console.log('working');
-  playerCompScore.removeChild(roundCount);
+  roundContainer.appendChild(roundCountPlaceholder);
+  roundContainer.removeChild(roundCount);
   playerCompScore.removeChild(playerResult);
-  playerCompScore.removeChild(computerResult);
-  score.removeChild(roundStatus);
+  // playerCompScore.removeChild(computerResult);
+  playerCompScore.removeChild(roundStatus);
   score.removeChild(playAgain);
   score.removeChild(gameResult);
   gameResult.textContent = '';
